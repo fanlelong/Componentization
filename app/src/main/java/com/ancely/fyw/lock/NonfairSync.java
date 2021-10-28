ed34ae9262d37c96a6b734b9631eafb5f519752d
ancely <761090890@qq.com>
2021-10-28 21:34:55 +0800

    Update NonfairSync.java
    "d"

--- app/src/main/java/com/ancely/fyw/lock/NonfairSync.java
+++ app/src/main/java/com/ancely/fyw/lock/NonfairSync.java
@@ -3,10 +3,6 @@ package com.ancely.fyw.lock;
 public class NonfairSync extends Sync {
     private static final long serialVersionUID = 7316153563782823691L;
 
-    /**
-     * Performs lock.  Try immediate barge, backing up to normal
-     * acquire on failure.
-     */
     final void lock() {
         if (compareAndSetState(0, 1))
             setExclusiveOwnerThread(Thread.currentThread());
